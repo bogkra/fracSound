8367ca2d22073335470fd1da09ee173a16c88efc
bogkra <bkrawczuk@wp.pl>
2018-04-17 09:36:59 +0200

    some autocommenting consts added

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,42 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
-namespace little_endian_io
-{
+namespace little_endian_io {
   template <typename Word>
-  std::ostream& write_word( std::ostream& outs, Word value, unsigned size = sizeof( Word ) )
-  {
+  std::ostream& write_word( std::ostream& outs, Word value, unsigned size = sizeof( Word ) ) {
     for (; size; --size, value >>= 8)
       outs.put( static_cast <char> (value & 0xFF) );
     return outs;
   }
 }
+
 using namespace little_endian_io;
 
-int main()
-{
+namespace Channels {
+  const int mono = 1;
+  const int stereo = 2;
+  const int surround = 5 + 1 ;
+}
+
+
+int main() {
+//http://www.cplusplus.com/user/Duthomhas/
   ofstream f( "example.wav", ios::binary );
+
+  int numberOfChannels = Channels::stereo ;  
+  const int bytesPerSample = 2;
+  const int bitesPerSample = 8*bytesPerSample;
+  const int samplesPerSecond = 44100;
 
   // Write the file headers
   f << "RIFF----WAVEfmt ";     // (chunk size to be filled in later)
   write_word( f,     16, 4 );  // no extension data
   write_word( f,      1, 2 );  // PCM - integer samples
-  write_word( f,      2, 2 );  // two channels (stereo file)
-  write_word( f,  44100, 4 );  // samples per second (Hz)
-  write_word( f, 176400, 4 );  // (Sample Rate * BitsPerSample * Channels) / 8
-  write_word( f,      4, 2 );  // data block size (size of two integer samples, one for each channel, in bytes)
-  write_word( f,     16, 2 );  // number of bits per sample (use a multiple of 8)
+  write_word( f, numberOfChannels, 2 );  
+  write_word( f, samplesPerSecond, 4 );  
+  write_word( f, samplesPerSecond * bytesPerSample * numberOfChannels, 4 );  
+  write_word( f, numberOfChannels * bytesPerSample, 2 );  // data block size 
+  write_word( f, bitesPerSample, 2 );  
 
   // Write the data chunk header
   size_t data_chunk_pos = f.tellp();
@@ -40,18 +51,17 @@ int main()
   
   // Write the audio samples
   // (We'll generate a single C4 note with a sine wave, fading from left to right)
-  constexpr double two_pi = 6.283185307179586476925286766559;
-  constexpr double max_amplitude = 32760;  // "volume"
+  const double two_pi = 6.283185307179586476925286766559;
+  const double max_amplitude = 32760;  // "volume"
 
-  double hz        = 44100;    // samples per second
+  double hz        = samplesPerSecond;    
   double frequency = 261.626;  // middle C
   double seconds   = 2.5;      // time
 
-  int N = hz * seconds;  // total number of samples
-  for (int n = 0; n < N; n++)
-  {
-    double amplitude = (double)n / N * max_amplitude;
-    double value     = sin( (two_pi * n * frequency) / hz );
+  int length = hz * seconds;  // total number of samples
+  for (int i = 0; i < length; i++) {
+    double amplitude = (double)i / length * max_amplitude;
+    double value     = sin( (two_pi * i * frequency) / hz );
     write_word( f, (int)(                 amplitude  * value), 2 );
     write_word( f, (int)((max_amplitude - amplitude) * value), 2 );
   }
